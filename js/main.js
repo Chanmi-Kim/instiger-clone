e7ea32cd682eb7061097661e0609fe96e0f95f6a
Yian Kim <devyiankim@gmail.com>
2021-11-13 23:57:44 +0900

    feat: [#8] Add event delegation - middle commit

--- js/main.js
+++ js/main.js
@@ -4,10 +4,36 @@ const sidebox = document.querySelector('.side_box');
 // SelectorAll을 활용해 모든 요소를 가져옴
 const variableWidth = document.querySelectorAll(".contents_box .contents");
 
-heart.addEventListener('click', function(){
-    console.log('hit');
-    heart.classList.toggle('on'); // 하트를 클릭하면 .on 클래스를 추가
-});
+// heart.addEventListener('click', function(){
+//     console.log('hit');
+//     heart.classList.toggle('on'); // 하트를 클릭하면 .on 클래스를 추가
+// });
+
+function deligationFunc(e){
+    let elem = e.target; // 클릭한 요소 가져오기
+
+    // 잘못 클릭한 경우
+    while(!elem.getAttribute('data-name')){
+        // elem의 부모를 찾음
+        elem = elem.parentNode;
+        if(elem.nodeName === 'BODY'){ // body까지 이벤트가 없는 경우
+            elem = null;
+            return;
+        } // data-name을 가진 속성을 찾을 때까지 부모에게 접근을 반복
+    }
+
+    if(elem.matches('[data-name="heartbeat')){
+        console.log("하트!");
+    }else if(elem.matches('[data-name="bookmark"')){
+        console.log("북마크!")''
+    }else if(elem.matches('[data-name="share"')){
+        console.log("공유!");
+    }if(elem.matches('[data-name="more"')){
+        console.log("더보기!");
+    }
+
+    elem.classList.toggle('on'); // on 클래스 추가
+}
 
 function resizeFunc(){
     if(pageYOffset >= 10){
@@ -40,5 +66,6 @@ function scrollFunc(){
     }
 }
 
+deligationFunc.addEventListener('click', deligationFunc);
 window.addEventListener('resize', resizeFunc);
 window.addEventListener('scroll', scrollFunc); // 스크롤 이벤트 발생 시 scrollFunc 실행
